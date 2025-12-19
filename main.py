42adde8629aaf15f47b768388f79aaf136b8f387
Rohit_More <morer4851@gmail.com>
2025-12-19 17:39:53 +0530

    Populated table structure with data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, \
-	QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget
+	QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow, QTableWidget, \
+	QTableWidgetItem
 
 from PyQt6.QtGui import QAction
-
 import sys
+import sqlite3
 
 # QMainWindow allows to create multiple interconnected windows
 # While QWidget only allows us to create a single one
@@ -29,14 +30,26 @@ class MainWindow(QMainWindow):
 		self.table.setHorizontalHeaderLabels((
 			"ID", "Name", "Course", "Mobile"
 		))
+		self.table.verticalHeader().setVisible(False)
 		self.setCentralWidget(self.table)
 		
 	def load_data(self):
-		pass
+		connection = sqlite3.connect("database.db")
+		result = connection.execute("SELECT * FROM students")
+		
+		self.table.setRowCount(0)
+		for row_number, row_data in enumerate(result):
+			self.table.insertRow(row_number)
+			for column_number, data in enumerate(row_data):
+				print(row_data)
+				self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+				
+		connection.close()
 		
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	main_window = MainWindow()
 	main_window.show()
+	main_window.load_data()
 	sys.exit(app.exec())
